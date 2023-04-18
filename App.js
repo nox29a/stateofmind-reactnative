32146ec7a891e8e65ced9fdbec58f5b57287dae5
nox29a <adamczyk29a@interia.pl>
2023-04-18 22:06:39 +0200

    fix bugs

--- App.js
+++ App.js
@@ -1,37 +1,61 @@
-
 import React, {useState, useEffect} from 'react';
-import {Calendar, LocaleConfig} from 'react-native-calendars';
-import CustomMultiPicker from "react-native-multiple-select-list";
-import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {Calendar} from 'react-native-calendars';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, Textarea, View } from 'react-native';
 
-const ok = {key: 'OK', color: 'brown'};
-const odrealnienie = {key: 'Odrealnienie', color: 'yellow'};
-const stres = {key: 'Stres', color: 'lightblue'};
-const emocje = {key: 'Dużo emocji', color: 'lightgreen'};
-const spokojnie = {key: 'Spokojnie', color: 'darkorange'};
-const zmęczenie = {key: 'Zmęczenie', color: 'pink'};
+const ok = {key: 'OK', color: 'brown', pressed: false};
+const odrealnienie = {key: 'Odrealnienie', color: 'yellow', pressed: false};
+const stres = {key: 'Stres', color: 'lightblue', pressed: false};
+const emocje = {key: 'Dużo emocji', color: 'lightgreen', pressed: false};
+const spokojnie = {key: 'Spokojnie', color: 'darkorange', pressed: false};
+const zmęczenie = {key: 'Zmęczenie', color: 'pink', pressed: false};
 const userList = [ok, odrealnienie, stres, emocje, spokojnie,zmęczenie]
 
 const App = () => {
   const [selected, setSelected] = useState("new Date()");
   const [marked, setMarked] = useState();
   const [mindState, setMindState] = useState([]);
+  const [check, setCheck] = useState();
 
   useEffect(() => {
     newTable = mindState;
+    
   });
 
   const handlePress = (item) => {
-    const mindState = newTable.concat(item);
-    const unique = [...new Set(mindState)];
-    setMindState(unique);
-  }
-  const handleSave = () => {
-    setMarked({...marked,
-      [selected] : {
-        dots: newTable
-      }})
+    console.log(item);
+    item.pressed =!item.pressed;
+    if (item.pressed) {
+      const mindState = newTable.concat(item);
+      const unique = [...new Set(mindState)];
+      setMindState(unique);
     }
+    else {
+      const unique = mindState.filter(test => test.key !== item.key);
+      setMindState(unique);
+    }}
+
+  const handleReset = () => {
+      {userList.map((item) => {item.pressed = false})}
+      setMindState([]);
+    }
+
+  const handleRead = (item) => {
+    {userList.map((test) => {test.pressed = false;
+    item.map((elem,index) => {  
+      console.log(index)
+      if ( test.key === elem[0].key) {
+        console.log("sadasda");
+        test.pressed = true}
+        setMindState([]);})})}
+  }
+
+  const handleSave = () => {
+      setMarked({...marked,
+        [selected] : {
+          dots: newTable
+        }})
+    }
+
   const handleDelete = () => {
       setMarked({...marked,
         [selected] : {
@@ -39,56 +63,61 @@ const App = () => {
         }})
       }
 
+
+
   return (
     <SafeAreaView style={styles.container}>
-    <Calendar
-    markingType="multi-dot"
-
-    markedDates={marked}
-    onDayPress={day => {
-        setMindState([]);
-        setSelected(day.dateString);
-      }}
-    
-    />
-      <Text style={styles.text}>{selected}</Text>
+      <Calendar
+        markingType="multi-dot"
+        const markedDotDates={marked}
+        onDayPress={day => {
+        setSelected(day.dateString)
+        if ((marked) === undefined){
+          console.log("marked is undefined")}
+        else {
+        if ((marked[day.dateString]) === undefined) {
+          handleReset()
+        }
+        else {
+          console.log("@@@@@@@@@@@@@@@@@@@@@@")
+          handleRead((marked[day.dateString]['dots']))
+        }
+          }}}
+        markedDates={{
+            ...marked,
+            [selected]: {selected: true}
+          }}
+      />
       <View style={styles.states}>
-      {userList.map((item) => {
-        return (
-        
-          <TouchableOpacity onPress={() => handlePress(item)}>
-            <Text style={{backgroundColor: item.color,
-                  fontSize: 20,
-                  textAlign: 'center',
-                  margin: 12,
-                  height: 40,
-                  margin: 12,
-                  borderWidth: 1,
-                  padding: 10,}}
-                  >
-                  {item.key}
-            </Text>
-          </TouchableOpacity>
-       
-          
-        );
-      })}
-    </View>
-    <TouchableOpacity onPress={() => handleSave()}>
-      <Text style={styles.text}>Zapisz</Text>
-    </TouchableOpacity>
-    <TouchableOpacity onPress={() => handleDelete()}>
-      <Text style={styles.text}>Usuń</Text>
-    </TouchableOpacity>
+        {userList.map((item) => {
+          return (  
+            <TouchableOpacity key={item.key} onPress={() => handlePress(item)} >
+              <Text style={{backgroundColor: item.pressed? item.color : 'white', 
+                          fontSize: 20,
+                          textAlign: 'center',
+                          margin: 12,
+                          height: 40,
+                          margin: 12,
+                          borderWidth: 1,
+                          padding: 10,}}
+                    >{item.key}
+              </Text>
+ 
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+      <TouchableOpacity onPress={() => handleSave()}> 
+          <Text style={styles.text}>Zapisz</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => handleDelete()}>
+          <Text style={styles.text}>Usuń</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
 
 export default App;
-
-// style:
-// <View style={styles.container}></View>
-// import { StyleSheet, Text, View } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -107,6 +136,14 @@ const styles = StyleSheet.create({
     borderWidth: 5,
     borderColor: 'red',
   },
+  notes: {
+    flex: 1,
+    justifyContent: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: 0,
+    borderColor: 'green',
+  },
   text: {
     fontSize: 20,
     color: 'white',
@@ -118,5 +155,4 @@ const styles = StyleSheet.create({
     padding: 10,
     borderColor: 'blue',
   },
-
 });
